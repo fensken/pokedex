ff267760914620c697807c2e646c7deb48c5c425
sKen <57618183+fensken@users.noreply.github.com>
2025-12-26 16:36:47 +0545

    feat: screen update.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -28,7 +28,6 @@ export default function RootLayout() {
         options={{
           title: "Pokemon Details",
           headerBackButtonDisplayMode: "minimal",
-          presentation: "modal",
         }}
       />
     </Stack>
